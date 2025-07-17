25c88c950a96db57ebe9951110dcef2c4b937aae
Mubashir Hussain <mubashir.connect@gmail.com>
2025-07-17 15:23:50 +0100

    work on image

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,44 +11,46 @@ $(document).ready(function () {
   });
 
 
-  const sharedSecret = 'a9f1c2e741f1cd9e27f839a98fcb82715e4f30c961b879a69f2e13e3a4a6d84b'; // same secret as backend
-  async function generateHMACSignature(secret, timestamp) {
-    const encoder = new TextEncoder();
-    const keyData = encoder.encode(secret);
-    const message = encoder.encode(timestamp.toString());
+  // const sharedSecret = 'a9f1c2e741f1cd9e27f839a98fcb82715e4f30c961b879a69f2e13e3a4a6d84b'; // same secret as backend
+  // async function generateHMACSignature(secret, timestamp) {
+  //   const encoder = new TextEncoder();
+  //   const keyData = encoder.encode(secret);
+  //   const message = encoder.encode(timestamp.toString());
 
-    const key = await crypto.subtle.importKey(
-      'raw',
-      keyData,
-      { name: 'HMAC', hash: 'SHA-256' },
-      false,
-      ['sign']
-    );
-    const sigBuffer = await crypto.subtle.sign('HMAC', key, message);
-    return Array.from(new Uint8Array(sigBuffer))
-      .map(b => b.toString(16).padStart(2, '0'))
-      .join('');
-  }
+  //   const key = await crypto.subtle.importKey(
+  //     'raw',
+  //     keyData,
+  //     { name: 'HMAC', hash: 'SHA-256' },
+  //     false,
+  //     ['sign']
+  //   );
+  //   const sigBuffer = await crypto.subtle.sign('HMAC', key, message);
+  //   return Array.from(new Uint8Array(sigBuffer))
+  //     .map(b => b.toString(16).padStart(2, '0'))
+  //     .join('');
+  // }
 
 let isSearching = false;
-  if (isSearching) return; // prevent double calls
-  isSearching = true;
+
   async function handleSearch() {
+      if (isSearching) return; // prevent double calls
+      isSearching = true;
+
     const asin = $('#asinInput').val().trim();
     if (!asin) {
       alert('Please enter an ASIN');
       return;
     }
 
-    const timestamp = Date.now();
-    const signature = await generateHMACSignature(sharedSecret, timestamp);
+    // const timestamp = Date.now();
+    // const signature = await generateHMACSignature(sharedSecret, timestamp);
     $.ajax({
       url: `https://liveprojects.online/asinradar/keepa.php?asin=${asin}`,
       method: 'GET',
-      headers: {
-        'X-TIMESTAMP': timestamp.toString(),
-        'X-SIGNATURE': signature
-      },
+      // headers: {
+      //   'X-TIMESTAMP': timestamp.toString(),
+      //   'X-SIGNATURE': signature
+      // },
       success: function (data) {
         $('#result').text(JSON.stringify(data, null, 2));
         handleKeepaResponse(data);
@@ -72,16 +74,15 @@ let isSearching = false;
   });
 
   function handleKeepaResponse(data) {
-      if (data.products && data.products.length > 0) {
-        const product = data.products[0];
-        const asin = product.asin;
-        const title = product.title;
+      if (data.asin && data.asin.length > 0) {
+        const product = data.image;
+        const asin = data.asin;
+        const title = data.title;
         var imageUrl = getProductImage(product);
         const img = document.createElement('img');
         img.src = imageUrl;
         img.alt = product.title;
         img.style.width = '300px'; // Optional: set image size
-
         // Append image to container div
         document.getElementById('imageContainer').appendChild(img);
  
